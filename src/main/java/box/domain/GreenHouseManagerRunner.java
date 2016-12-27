636257a3d3013fe3714a2e8e4ba57c6e9a16b098
majchrzamemil <majchrzakemil@gmail.com>
2016-12-27 16:23:26 +0100

    @configurable

--- src/main/java/box/domain/GreenHouseManagerRunner.java
+++ src/main/java/box/domain/GreenHouseManagerRunner.java
@@ -8,9 +8,10 @@ import org.springframework.stereotype.Component;
 import box.repository.GreenHouseManagerRepository;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.stereotype.Service;
 
-@Component
+@Configurable(preConstruction = true)
 public class GreenHouseManagerRunner extends Thread {
 
     @Autowired
