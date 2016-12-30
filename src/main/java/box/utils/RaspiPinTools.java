2b2c493de89719fd5a382d873e7eda03a2c5522e
majchrzamemil <majchrzakemil@gmail.com>
2016-12-30 14:04:44 +0100

    first way we will see

--- src/main/java/box/utils/RaspiPinTools.java
+++ src/main/java/box/utils/RaspiPinTools.java
@@ -15,11 +15,8 @@ import com.pi4j.wiringpi.Gpio;
  */
 public class RaspiPinTools {
 
-    public static native float[] readData(int pin);
-
-    static {
-        System.loadLibrary("dht11sensor");
-    }
+    private static final int MAXTIMINGS = 85;
+    private static int[] dht11_dat = {0, 0, 0, 0, 0};
 
     public static Pin getEnumFromInt(int pinNumber) {
 
@@ -45,8 +42,68 @@ public class RaspiPinTools {
         }
     }
 
+    private static boolean checkParity() {
+        return (dht11_dat[4] == ((dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF));
+    }
+
     public static double getHumidity(int pinNumber) {
-        float[] data = RaspiPinTools.readData(pinNumber);
-        return data[0];
+
+        int laststate = Gpio.HIGH;
+        int j = 0;
+        dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
+
+        Gpio.pinMode(pinNumber, Gpio.OUTPUT);
+        Gpio.digitalWrite(pinNumber, Gpio.LOW);
+        Gpio.delay(18);
+
+        Gpio.digitalWrite(pinNumber, Gpio.HIGH);
+        Gpio.pinMode(pinNumber, Gpio.INPUT);
+
+        for (int i = 0; i < MAXTIMINGS; i++) {
+            int counter = 0;
+            while (Gpio.digitalRead(pinNumber) == laststate) {
+                counter++;
+                Gpio.delayMicroseconds(1);
+                if (counter == 255) {
+                    break;
+                }
+            }
+
+            laststate = Gpio.digitalRead(pinNumber);
+
+            if (counter == 255) {
+                break;
+            }
+
+            /* ignore first 3 transitions */
+            if ((i >= 4) && (i % 2 == 0)) {
+                /* shove each bit into the storage bytes */
+                dht11_dat[j / 8] <<= 1;
+                if (counter > 16) {
+                    dht11_dat[j / 8] |= 1;
+                }
+                j++;
+            }
+        }
+        // check we read 40 bits (8bit x 5 ) + verify checksum in the last
+        // byte
+//        if ((j >= 40) && checkParity()) {
+            float h = (float) ((dht11_dat[0] << 8) + dht11_dat[1]) / 10;
+            if (h > 100) {
+                h = dht11_dat[0];   // for DHT11
+            }
+//            float c = (float) (((dht11_dat[2] & 0x7F) << 8) + dht11_dat[3]) / 10;
+//            if (c > 125) {
+//                c = dht11_dat[2];   // for DHT11
+//            }
+//            if ((dht11_dat[2] & 0x80) != 0) {
+//                c = -c;
+//            }
+//            float f = c * 1.8f + 32;
+            return h;
+//        } else {
+//            return -999;
+//        }
+
     }
 }
